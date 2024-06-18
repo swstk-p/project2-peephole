e26ed2d8905fbdbffa5ce80b6ceafa3ea502f164
Swastik Pandey <swastik.pandey@deerwalk.edu.np>
2024-06-18 11:08:59 +0545

    feature: camera accessed within interface

---
+++ extension/public/scripts/components/FaceCam.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect, useRef } from "react";
+
+function FaceCam() {
+  const [faceStream, setFaceStream] = useState(null);
+  const videoRef = useRef(null); //setting a reference object to assign later to video element
+
+  useEffect(() => {
+    // to get stream and assign its data to our component state
+    async function getCamStream() {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+        });
+        setFaceStream(stream);
+      } catch (err) {
+        console.log(err.message);
+      }
+    }
+
+    getCamStream();
+  }, []); //empty array so that this useEffect is run only once
+
+  useEffect(() => {
+    // if we have vid element identified and stream also set
+    if (videoRef.current && faceStream) {
+      videoRef.current.srcObject = faceStream;
+    }
+  }, [faceStream]);
+  return (
+    <>
+      <div className="flex items-center justify-center w-80 h-80 rounded-lg border border-purple-400">
+        <video ref={videoRef} autoPlay></video> //doesn't work if it's autoplay
+        instead of autoPlay
+      </div>
+    </>
+  );
+}
+
+export default FaceCam;
--- extension/public/scripts/components/MainBody.jsx
+++ extension/public/scripts/components/MainBody.jsx
@@ -1,7 +1,11 @@
+import FaceCam from "./FaceCam";
+
 function MainBody() {
   return (
     <>
-      <div className="w-full h-96 rounded-lg border border-yellow-400"></div>
+      <div className="flex items-center justify-center w-full h-96 rounded-lg border border-yellow-400">
+        <FaceCam />
+      </div>
     </>
   );
 }
