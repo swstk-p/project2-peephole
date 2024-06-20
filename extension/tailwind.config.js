55e14bcee9df23faa2722e5f8506ee06f55d4286
Swastik Pandey <swastik.pandey@deerwalk.edu.np>
2024-06-20 14:29:11 +0545

    style: show modified logo font

--- extension/tailwind.config.js
+++ extension/tailwind.config.js
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./public/scripts/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      height: {
-        ext: "600px",
-      },
-      width: {
-        ext: "400px",
-      },
+    content: ["./index.html", "./public/scripts/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            height: {
+                ext: "600px",
+            },
+            width: {
+                ext: "400px",
+            },
+            fontFamily: {
+                logo: ['"Echo Station"'],
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
