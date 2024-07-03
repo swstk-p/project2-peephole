7763c2d5279e94d5ba81c38655d1921131ff96e5
Swastik Pandey <swastik.pandey@deerwalk.edu.np>
2024-07-03 18:50:05 +0545

    debug: show second bounding box to determine correct alignment

--- extension/public/scripts/components/FaceCam.jsx
+++ extension/public/scripts/components/FaceCam.jsx
@@ -7,13 +7,14 @@ function FaceCam() {
   const [faceDetection, setFaceDetection] = useState(undefined);
   const videoRef = useRef(null); //setting a reference object to assign later to video element
   const canvasRef = useRef(null);
-  const imgSizes = [32, 64, 96, 128, 160, 192, 224, 256];
+  const canvasRef2 = useRef(null);
   useEffect(() => {
     // to get stream
     async function getCamStream() {
       try {
         const stream = await navigator.mediaDevices.getUserMedia({
-          video: true,
+          video: { width: 1280, height: 720 },
+          audio: false,
         });
         setFaceStream(stream);
       } catch (err) {
@@ -36,6 +37,8 @@ function FaceCam() {
     // to load models
     async function loadModel() {
       await faceapi.nets.tinyFaceDetector.loadFromUri("../../models");
+      await faceapi.nets.faceLandmark68Net.loadFromUri("../../models");
+      await faceapi.nets.faceLandmark68TinyNet.loadFromUri("../../models");
     }
     loadModel();
   }, []);
@@ -45,13 +48,16 @@ function FaceCam() {
     async function detectFace() {
       if (videoRef.current && faceStream && videoRef.current.readyState === 4) {
         try {
-          for (const size of imgSizes) {
-            const detection = await faceapi.detectSingleFace(
+          const detection = await faceapi
+            .detectSingleFace(
               videoRef.current,
-              new faceapi.TinyFaceDetectorOptions({ inputSize: size })
-            );
-            setFaceDetection(detection);
-          }
+              new faceapi.TinyFaceDetectorOptions({
+                size: 96,
+                scoreThreshold: 0.3,
+              })
+            )
+            .withFaceLandmarks(true);
+          setFaceDetection(detection);
         } catch (err) {
           console.log(`DETECTION ERROR: ${err}`);
         }
@@ -62,7 +68,7 @@ function FaceCam() {
     if (faceStream) {
       const interval = setInterval(() => {
         detectFace();
-      }, 50);
+      }, 75);
       // to clean up interval on component unload
       return () => clearInterval(interval);
     }
@@ -75,12 +81,17 @@ function FaceCam() {
       const vidHeight = videoRef.current.getBoundingClientRect().height;
       canvasRef.current.width = vidWidth;
       canvasRef.current.height = vidHeight;
+      canvasRef2.current.width = vidWidth;
+      canvasRef2.current.height = vidHeight;
     }
   }, [videoRef, canvasRef]);
 
   useEffect(() => {
     // erase rectangle if face detection changes
     canvasRef.current
+      .getContext("2d")
+      .clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+    canvasRef2.current
       .getContext("2d")
       .clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
     // display canvas if face detected
@@ -90,13 +101,20 @@ function FaceCam() {
         width: videoRef.current.getBoundingClientRect().width,
         height: videoRef.current.getBoundingClientRect().height,
       });
+      // logging
+      console.log("RESIZED:", resizedDetection);
       // create a DrawBox obj using resizedDetection's box and DrawBoxOptions
       const drawBox = new faceapi.draw.DrawBox(
-        resizedDetection.box,
-        new DrawBoxOptions({ boxColor: "#5efc03", lineWidth: 2 })
+        resizedDetection.alignedRect.box,
+        new DrawBoxOptions({ boxColor: "#5efc03", lineWidth: 1 })
+      );
+      const drawBox2 = new faceapi.draw.DrawBox(
+        resizedDetection.detection.box,
+        new DrawBoxOptions({ boxColor: "#ffffff", lineWidth: 1 })
       );
       // draw on the canvas with drawBox
       drawBox.draw(canvasRef.current);
+      drawBox2.draw(canvasRef2.current);
     }
   }, [faceDetection]);
 
@@ -104,13 +122,22 @@ function FaceCam() {
     <>
       <div className="relative flex items-center justify-center w-full h-full rounded-lg">
         <video
-          className="flex w-full aspect-square object-none rounded-lg z-0"
+          className="flex w-full aspect-square object-cover rounded-lg z-0"
           ref={videoRef}
           autoPlay></video>
         <canvas className="rounded-lg absolute z-10" ref={canvasRef}></canvas>
+        <canvas className="rounded-lg absolute z-10" ref={canvasRef2}></canvas>
       </div>
     </>
   );
 }
 
 export default FaceCam;
+
+/* 
+  STEPS FOR BOUNDING BOX
+  1. declare a ref
+  2. put a canvas element and useRef
+  3. set canvas size and position (effect depends on stream and ref value)
+  4. clear canvas -> resize detection -> create a box -> draw the box (effect depends on detection value)
+*/
