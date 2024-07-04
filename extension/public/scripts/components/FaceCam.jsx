bbb87a1658a4f77876b939677f032a937ee0e35e
Swastik Pandey <swastik.pandey@deerwalk.edu.np>
2024-07-04 11:06:29 +0545

    feature: draw detection box completely surrounding face with the face in the middle

--- extension/public/scripts/components/FaceCam.jsx
+++ extension/public/scripts/components/FaceCam.jsx
@@ -7,7 +7,33 @@ function FaceCam() {
   const [faceDetection, setFaceDetection] = useState(undefined);
   const videoRef = useRef(null); //setting a reference object to assign later to video element
   const canvasRef = useRef(null);
-  const canvasRef2 = useRef(null);
+
+  function adjustBox() {
+    const boxSize = 150;
+    const heightOffset = 50;
+    // resize detections to match video dimensions
+    const resizedDetection = faceapi.resizeResults(faceDetection, {
+      width: videoRef.current.getBoundingClientRect().width,
+      height: videoRef.current.getBoundingClientRect().height,
+    });
+    return new faceapi.draw.DrawBox(
+      new faceapi.Box(
+        new faceapi.BoundingBox(
+          resizedDetection.alignedRect.box.x -
+            parseFloat(boxSize - resizedDetection.alignedRect.box.width) / 2,
+          resizedDetection.alignedRect.box.y -
+            parseFloat(boxSize - resizedDetection.alignedRect.box.height) / 2 -
+            heightOffset,
+          resizedDetection.alignedRect.box.x + boxSize,
+          resizedDetection.alignedRect.box.y + boxSize,
+          false
+        ),
+        false
+      ),
+      new DrawBoxOptions({ boxColor: "#5efc03", lineWidth: 1.5 })
+    );
+  }
+
   useEffect(() => {
     // to get stream
     async function getCamStream() {
@@ -81,8 +107,6 @@ function FaceCam() {
       const vidHeight = videoRef.current.getBoundingClientRect().height;
       canvasRef.current.width = vidWidth;
       canvasRef.current.height = vidHeight;
-      canvasRef2.current.width = vidWidth;
-      canvasRef2.current.height = vidHeight;
     }
   }, [videoRef, canvasRef]);
 
@@ -91,49 +115,11 @@ function FaceCam() {
     canvasRef.current
       .getContext("2d")
       .clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-    canvasRef2.current
-      .getContext("2d")
-      .clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
     // display canvas if face detected
     if (faceDetection !== undefined) {
-      // resize detections to match video dimensions
-      const resizedDetection = faceapi.resizeResults(faceDetection, {
-        width: videoRef.current.getBoundingClientRect().width,
-        height: videoRef.current.getBoundingClientRect().height,
-      });
-      // logging
-      console.log("RESIZED:", resizedDetection);
-      console.log("Box_x:", resizedDetection.alignedRect.box.x);
-      console.log(
-        "Constructor parameters:",
-        resizedDetection.detection.box.x,
-        resizedDetection.detection.box.y,
-        resizedDetection.detection.box.x + resizedDetection.detection.box.width,
-        resizedDetection.detection.box.y + resizedDetection.detection.box.height
-      );
-      // create a DrawBox obj using resizedDetection's box and DrawBoxOptions
-      const drawBox = new faceapi.draw.DrawBox(
-        resizedDetection.alignedRect.box,
-        new DrawBoxOptions({ boxColor: "#5efc03", lineWidth: 1 })
-      );
-      const drawBox2 = new faceapi.draw.DrawBox(
-        new faceapi.Box(
-          new faceapi.BoundingBox(
-            resizedDetection.detection.box.x,
-            resizedDetection.detection.box.y,
-            resizedDetection.detection.box.x +
-              resizedDetection.detection.box.width,
-            resizedDetection.detection.box.y +
-              resizedDetection.detection.box.height,
-            false
-          ),
-          false
-        ),
-        new DrawBoxOptions({ boxColor: "#ffffff", lineWidth: 1 })
-      );
+      const drawBox = adjustBox();
       // draw on the canvas with drawBox
       drawBox.draw(canvasRef.current);
-      drawBox2.draw(canvasRef2.current);
     }
   }, [faceDetection]);
 
@@ -145,7 +131,6 @@ function FaceCam() {
           ref={videoRef}
           autoPlay></video>
         <canvas className="rounded-lg absolute z-10" ref={canvasRef}></canvas>
-        <canvas className="rounded-lg absolute z-10" ref={canvasRef2}></canvas>
       </div>
     </>
   );
@@ -158,5 +143,5 @@ export default FaceCam;
   1. declare a ref
   2. put a canvas element and useRef
   3. set canvas size and position (effect depends on stream and ref value)
-  4. clear canvas -> resize detection -> create a box -> draw the box (effect depends on detection value)
+  4. clear canvas -> adjust box -> draw the box (effect depends on detection value)
 */
